e42b1cee3d8db7f290a6f60a145a1492bd6fd1cf
Avashist1998 <36799410+Avashist1998@users.noreply.github.com>
2024-02-22 21:40:56 -0600

    Error correction (#20)
    * fix: infinite calls correction
    
    * fix: record page and bug fix

--- ui/src/components/NavigationBar.tsx
+++ ui/src/components/NavigationBar.tsx
@@ -1,15 +1,32 @@
 import { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import TimelineIcon from '@mui/icons-material/Timeline';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 
-
 const NavigationBar = () => {
 
-    const [value, setValue] = useState(0);
+    const navigate = useNavigate();
+    const location = useLocation();
 
+    const getActiveTab = () => {
+        if (location.pathname === "/") {
+            return 0;
+        } else if (location.pathname === "/record") {
+            return 1;
+        } else if (location.pathname === "/nearby") {
+            return 2;
+        }
+        return 0;
+    }
+
+    const [value, setValue] = useState(getActiveTab());
+
+    const navigateTo = (path: string) => {
+        navigate(path);
+    }
 
     return (
         <BottomNavigation
@@ -19,9 +36,9 @@ const NavigationBar = () => {
                 console.log("This was triggered")
                 setValue(newValue);
             }}>
-            <BottomNavigationAction label="Home" icon={<TimelineIcon />} href="./"/>
-            <BottomNavigationAction label="Record" icon={<RadioButtonCheckedIcon />} />
-            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+            <BottomNavigationAction label="Home" icon={<TimelineIcon />} onClick={()=>{navigateTo("/")}}/>
+            <BottomNavigationAction label="Record" icon={<RadioButtonCheckedIcon />} onClick={()=>{navigateTo("/record")}}/>
+            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} onClick={()=>{navigateTo("/nearby")}}/>
         </BottomNavigation>
     )
 }
